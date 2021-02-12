fc09a9b23f5ae33a673444d63cd933d0b96d0043
Jerrod Styrk <jstyrk@citadel.edu>
2021-02-12 09:16:19 -0800

    build out the keg display and constructor

---
+++ src/components/Keg.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Keg(props) {
+  return (
+    <>
+      <div className='card' onClick= {() => props.whenKegClicked(props.id)}>
+        <div className='card-header'>{props.name}</div>
+        <div className='card-body'>${props.price}</div>
+        <div className='card-footer'>{props.pints} serving/s in stock</div>
+      </div>
+    </>
+  );
+}
+
+Keg.propTypes = {
+  name: PropTypes.string.isRequired,
+  brand: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  alcoholContent: PropTypes.number.isRequired,
+  pints: PropTypes.number.isRequired,
+  id: PropTypes.string,
+  whenKegClicked: PropTypes.func
+};
+
+export default Keg;
