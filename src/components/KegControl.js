b0046fdf0bc2a404b5afbb49d5bec1109ae4cace
Jerrod Styrk <jstyrk@citadel.edu>
2021-02-12 11:57:29 -0800

    start building out Keg Control

---
+++ src/components/KegControl.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import KegList from './KegList';
+import NewKegForm from './NewKegForm';
+import KegDetail from './KegDetail';
+
+class KegControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false,
+      masterKegList: [],
+      selectedKeg: null
+    };
+  }
+
+  handleClick = () => {
+    if (this.state.selectedKeg != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedKeg: null
+      });
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage
+      }));
+    }
+  }
+
+  
+}
