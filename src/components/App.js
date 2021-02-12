2f5b99f2c19b7a8eaa30af88aa9b67b90aa072cd
Jerrod Styrk <jstyrk@citadel.edu>
2021-02-12 08:42:10 -0800

    install bootstrap and build out App.js

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import KegControl from './KegControl';
+
+function App() {
+  return (
+    <>
+      <div className='container'>
+        <KegControl />
+      </div>
+    </>
+  );
+}
+
+export default App;
