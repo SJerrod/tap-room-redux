bc1f96740345938d6de41482c41b559427af5a61
Jerrod Styrk <jstyrk@citadel.edu>
2021-02-19 14:50:00 -0800

    successfully pass test to check initial state against rootReducer state

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,4 +1,9 @@
 import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import kegListReducer from '../../reducers/keg-list-reducer';
+
+let store = createStore(rootReducer);
 
 describe('rootReducer', () => {
   test('Should return default state if no action type is recognized', () => {
@@ -6,5 +11,13 @@ describe('rootReducer', () => {
       masterKegList: {},
       formVisibleOnPage: false
     });
+  });
+
+  test('Check if initial state of kegListReducer matches the rootReducer', () => {
+    expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, { type: null }));
+  });
+
+  test('Check if initial state of formVisibleReducer matches the rootReducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
   });
 });
