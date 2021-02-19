5ff6461bb3ab30bdb5741be13b6cf719e827d596
Jerrod Styrk <jstyrk@citadel.edu>
2021-02-19 09:27:53 -0800

    successfully pass ADD_KEG test

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,19 @@
 export default (state = {}, action) => {
-  return state;
+  const { name, brand, description, price, alcoholContent, pints, id } = action;
+  switch (action.type) {
+    case 'ADD_KEG':
+      return Object.assign({}, state, {
+        [id] : {
+          name: name,
+          brand: brand,
+          description: description,
+          price: price,
+          alcoholContent: alcoholContent,
+          pints: pints,
+          id: id
+        }
+      });
+    default:
+      return state;
+  }
 };
