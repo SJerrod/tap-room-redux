bf50234dd61e84d29af1e3febe9740b25aa5ce08
Jerrod Styrk <jstyrk@citadel.edu>
2021-02-19 16:23:46 -0800

    successfully pass test detail-keg-reducer with action BUY_PINT

--- src/__tests__/reducers/detail-keg-reducer.test.js
+++ src/__tests__/reducers/detail-keg-reducer.test.js
@@ -18,4 +18,29 @@ describe('detailKegReducer', () => {
   test('Should return default state if no action type is recognized', () => {
     expect(detailKegReducer({}, { type: null })).toEqual({});
   });
-})
+
+  test('Should return selected keg when BUY_PINT action', () => {
+    const { name, brand, description, price, alcoholContent, pints, id } = currentState;
+    action = {
+      type: 'BUY_PINT',
+      name: name,
+      brand: brand,
+      description: description,
+      price: price,
+      alcoholContent: alcoholContent,
+      pints: pints,
+      id: id
+    };
+    expect(detailKegReducer(currentState, { type: 'BUY_PINT'})).toEqual({
+      [id] : {
+        name: name,
+        brand: brand,
+        description: description,
+        price: price,
+        alcoholContent: alcoholContent,
+        pints: pints,
+        id: id
+      }
+    });
+  });
+});
